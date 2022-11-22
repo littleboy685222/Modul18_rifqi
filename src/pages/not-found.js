810878635d87f47488f1f9d7049df4215162f36b
Muhammad Rifqi Fauzan <91310558+littleboy685222@users.noreply.github.com>
2022-11-22 16:31:33 +0700

    Add files via upload

---
+++ src/pages/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+// Modifikasi halaman berikut untuk menjadi halaman 404 yang menarik!
+const NotFound = () => {
+  return(
+    
+    <div class='error'>404 PAGE NOT FOUND</div>
+  )
+}
+
+export default NotFound
