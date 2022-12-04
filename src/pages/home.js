a43194f79c7a58b1dae1330d108a41f0a2aca43e
Muhammad Rifqi Fauzan <91310558+littleboy685222@users.noreply.github.com>
2022-12-04 21:37:33 +0700

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -51,7 +51,7 @@ const Home = () => {
 
         const verify = async() =>{
             try {
-              const response = await axios.post('http://localhost:3001/verify', {
+              const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/verify`, {
                 token: localStorage.getItem('token')
               })
               if(response.status == 200){
